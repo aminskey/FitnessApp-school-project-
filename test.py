ad1d40f09369a911f07f66bce4cb34cc0b2fb78a
drminskey <drminskey@localhost.localdomain>
2021-05-04 13:06:50 +0200

    Adding changes

--- test.py
+++ test.py
@@ -9,7 +9,16 @@ clock = pygame.time.Clock()
 
 bg = pygame.image.load('bg.png')
 
+run = pygame.image.load('run.png')
+walk = pygame.image.load('walking.png')
+eat = pygame.image.load('eating.png')
+cycle = pygame.image.load('cycle.png')
+weight = pygame.image.load('weight.png')
+logger = pygame.image.load('logger.png')
+
+
 pygame.display.set_caption('Mobile Phone Emulator')
+
 
 while done != 1:
 	for event in pygame.event.get():
@@ -17,11 +26,24 @@ while done != 1:
 			done = 1
 			break
 		if event.type == pygame.MOUSEMOTION:
-			print("Yay")
+#			print("Yay")
 			break
 
+	# screen background color
 	screen.fill((118,130,121))
+
+
+	# screen background image
 	screen.blit(bg, (0, 0))
 
-	pygame.display.flip()
+	# apps
+	screen.blit(run, (40, 260))
+	screen.blit(walk, (400 * 3/4, 320))
+	screen.blit(eat, (400 * 3/16, 120))
+	screen.blit(cycle, (400 * 5/8, 170))
+	screen.blit(weight, (400 * 11/32, 400))
+	screen.blit(logger, (400 * 1/2, 800 * 3/8))
+
+
+	pygame.display.update()
 	clock.tick(75)
